b9c7385e18de45cb53ff7ba4e55d78bfcfde6b0b
Fayorin <126774711+Fayorin@users.noreply.github.com>
2025-09-15 23:56:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import Painter
 import numpy as np
-
+import math
 from Eiler import *
 from RosslerAttractor import RosslerAttractor
 from RungeKutta2 import RungeKutta2
 from RungeKutta4 import RungeKutta4
+from CD import CD
 
 
 def solve(metod, time, h, X, a, rossler, name):
@@ -21,19 +22,23 @@ def getDeviation(metod, time, h, X, a, rossler, name):
 
     # Разница между массивами
     difference = X2_np - X1_np
+    print(difference)
     Painter.draw(difference, h, name, True)
-    Painter.draw_3d(difference, name)
+    #Painter.draw_3d(difference, name)
 
 
 if __name__ == "__main__":
     rossler = RosslerAttractor()  # Создаем экземпляр класса
     X = [0.1, 0.1, 0.1]
-    a = [0, 0.2, 0.2, 5.7]
+    a = [0.5, 10, 28, 8/3]
     time = 100
-    h = 0.0001
-
+    h = 0.00001
     solve(Eiler(), time, h, X, a, rossler, "Метод эйлера")
     solve(RungeKutta2(), time, h, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка")
+    solve(CD(), time, h, X, a, rossler, "КД")
     solve(RungeKutta4(), time, h, X, a, rossler, "Классический метод Рунге — Кутты четвёртого порядка")
-    getDeviation(RungeKutta2(), time, h, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка")
-    getDeviation(Eiler(), time, h, X, a, rossler, "Метод эйлера")
+    getDeviation(Eiler(), time, h, X, a, rossler, "Метод эйлера – ошибка")
+    getDeviation(CD(), time, h, X, a, rossler, "КД – ошибка")
+    #getDeviation(CD(), time, 0.00001, X, a, rossler, "КД")
+    getDeviation(RungeKutta2(), time, 0.00001, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка– ошибка")
+    #getDeviation(RungeKutta2(), time, 0.000001, X, a, rossler, "Неявный метод Рунге — Кутты второго порядка– ошибка")
