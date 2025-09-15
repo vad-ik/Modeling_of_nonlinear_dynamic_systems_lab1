0d9178f61700019edef40763d57b606f40d9459a
Fayorin <126774711+Fayorin@users.noreply.github.com>
2025-09-15 23:56:25 +0300

    Update CD.py

--- CD.py
+++ CD.py
@@ -12,14 +12,16 @@ class CD:
             h1 = h * a[0]
             h2 = h * (1 - a[0])
 
-            tmp0 = X1[i][0] + h1 * func.getdX(X1[i], a)
-            tmp1 = X1[i][1] + h1 * func.getdY(X1[i], a)
-            tmp2 = X1[i][2] + h1 * func.getdZ(X1[i], a)
+            tmp0 = X1[i][0] + h1 * (a[1]*(X1[i][1] - X1[i][0]))
+            tmp1 = X1[i][1] + h1 * (tmp0*(a[2]-X1[i][2]) - X1[i][1])
+            tmp2 = X1[i][2] + h1 * (tmp0 * tmp1 - a[3] * X1[i][2])
 
 
-            X1[i + 1][2] = (tmp2 + h2 * a[2]) / (1 - h2 * (tmp0 - a[3]))
-            X1[i + 1][1] = (tmp1 + h2 * tmp0) / (1 - h2 * a[1])
-            X1[i + 1][0] = tmp0 + h2 * (-X1[i + 1][1] - X1[i + 1][2])
+
+            X1[i + 1][2] = (tmp2 + h2 * tmp0 * tmp1) / (1 + h2 * a[3])
+            X1[i + 1][1] = (tmp1 + h2 * tmp0*(a[2]- X1[i + 1][2])) / (1 + h2)
+            X1[i + 1][0] = (tmp0 + h2 * a[1] * X1[i + 1][1]) / (1 + h2 * a[1])
+
 
         X_coords, Y_coords, Z_coords = zip(*X1)
         return [list(X_coords), list(Y_coords), list(Z_coords)]
